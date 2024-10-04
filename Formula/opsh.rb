38d0defa31931bf478931e752456ececf41e721f
Alexander Guy <alexander.guy@pm.me>
2024-10-04 23:24:28 +0900

    Add a basic opsh formula

---
+++ Formula/opsh.rb
@@ -0,0 +1,16 @@
+class Opsh < Formula
+  desc "Operations Shell"
+  homepage "https://github.com/alexanderguy/opsh"
+  url "https://github.com/alexanderguy/opsh.git",
+      tag: "0.0.2"
+
+  depends_on "bash"
+
+  def install
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+
+  test do
+    system "true"
+  end
+end
