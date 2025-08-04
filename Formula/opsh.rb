dd62bf9a1d7acd1c3ea67aa1eb271a66c7e73e33
Alexander Guy <alexander.guy@pm.me>
2025-08-04 15:12:31 -0700

    Update to opsh v0.9.0

--- Formula/opsh.rb
+++ Formula/opsh.rb
@@ -2,7 +2,7 @@ class Opsh < Formula
   desc "Operations Shell"
   homepage "https://github.com/alexanderguy/opsh"
   url "https://github.com/alexanderguy/opsh.git",
-      tag: "0.8.0"
+      tag: "0.9.0"
 
   depends_on "bash"
 
